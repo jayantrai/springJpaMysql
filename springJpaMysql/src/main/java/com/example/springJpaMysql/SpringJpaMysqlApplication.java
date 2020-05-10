d87d49fd9274b69c3c60215a22005ac4643b3482
Jayant Rai <jayant.chamling@outlook.com>
2020-05-09 20:02:59 -0500

    authentication and authorization to connect to some database

---
+++ springJpaMysql/src/main/java/com/example/springJpaMysql/SpringJpaMysqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springJpaMysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaMysqlApplication.class, args);
+	}
+
+}
---
+++ springJpaMysql/src/test/java/com/example/springJpaMysql/SpringJpaMysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springJpaMysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaMysqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
